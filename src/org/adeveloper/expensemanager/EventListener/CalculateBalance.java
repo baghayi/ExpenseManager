75065d58d82284c8108794851b6d19e62fd3aa77
Hossein Baghayi <hossein.baghayi@gmail.com>
2013-09-29 02:38:08 +0330

    Manage resources UI made to be User friendly
    Instead of duplicate field & text & ... for adding and subtracting balance it all converted to be one and rather having a button to change the state of being addition or subtracted.
    
    Add dialogbox to ask for user's confirmation before adding or subtractin balance for error-prone.
    
    States will be stored when changing form portrit mode to landskape and vice-versa.
    
    Plus and Minus icons used in this section of app for chaning the Balance type:
    https://www.iconfinder.com/icons/126583/add_plus_icon#size=20
    https://www.iconfinder.com/icons/183199/minus_icon#size=20

--- src/org/adeveloper/expensemanager/EventListener/CalculateBalance.java
+++ src/org/adeveloper/expensemanager/EventListener/CalculateBalance.java
@@ -4,11 +4,14 @@ import org.adeveloper.expensemanager.Balance;
 import org.adeveloper.expensemanager.util.DatabaseConnectionFactory;
 import org.adeveloper.expensemanager.util.Notification;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public final class CalculateBalance implements OnClickListener
 {
@@ -19,37 +22,78 @@ public final class CalculateBalance implements OnClickListener
 	private Context context;
 	private String balanceType;
 	private EditText balanceField;
-	
+	private TextView realtimeBalance;
 
-	public CalculateBalance(Context context, String balanceType, EditText balanceField)
+	public CalculateBalance(Context context, String balanceType, EditText balanceField, TextView realtimeBalance)
 	{
 		this.context = context;
 		this.balanceType = balanceType;
 		this.balanceField = balanceField;
+		this.realtimeBalance = realtimeBalance;
+	}
+	
+	
+	private double getPrice(){
+		String price = balanceField.getText().toString();
+		
+		if(price.isEmpty()){
+			return 0;
+		}
+		
+		return Double.parseDouble(price);
 	}
 	
 	
 	@Override
 	public void onClick(View v)
 	{
-		SQLiteDatabase database = getDatabaseConnection();
-		Balance balance = new Balance(database);
-		
-		double newBalance =  Double.parseDouble(balanceField.getText().toString());
-		boolean result;
-		
-		if(balanceType == BALANCE_INCREASE){
-			 result = balance.add(newBalance);
-		}else if (balanceType == BALANCE_DECREASE){
-			result = balance.subtract(newBalance);
-		}else {
-			result = false;
+		if(getPrice() == 0){
+			return;
 		}
 		
-		database.close();
+		// TODO Ask user for confirmation.
+		AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+		alertBuilder.setMessage("آیا از انجام عمل مورد نظر اطمینان داری؟");
+		alertBuilder.setCancelable(false);
+		alertBuilder.setNegativeButton("نه نمی دونم!", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				dialog.cancel();
+			}
+		});
 		
-		Notification.successFailureToastMessage(context, result);
+		alertBuilder.setPositiveButton("آره داداش", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which)
+			{
+				SQLiteDatabase database = CalculateBalance.this.getDatabaseConnection();
+				Balance balance = new Balance(database);
+				
+				boolean result;
+				
+				if(balanceType == BALANCE_INCREASE){
+					result = balance.add(getPrice());
+				}else if (balanceType == BALANCE_DECREASE){
+					result = balance.subtract(getPrice());
+				}else {
+					result = false;
+				}
+				
+				database.close();
+				
+				Notification.successFailureToastMessage(context, result);
+				
+				if(result){
+					CalculateBalance.this.resetBalanceField();
+				}
+			}
+		});
 		
+		AlertDialog alert = alertBuilder.create();
+		alert.show();
 	}
 	
 	// TODO this method feels like a code smell!
@@ -57,6 +101,15 @@ public final class CalculateBalance implements OnClickListener
 	{
 		return new DatabaseConnectionFactory(context)
 		.create(DatabaseConnectionFactory.DATABASE_EXPENSECHEKER);
+	}
+	
+	private void resetBalanceField()
+	{
+		balanceField.setText("0");
+		
+		if(realtimeBalance != null){
+			realtimeBalance.setText("0");
+		}
 	}
 
 }
--- src/org/adeveloper/expensemanager/ManageResourcesActivity.java
+++ src/org/adeveloper/expensemanager/ManageResourcesActivity.java
@@ -5,11 +5,22 @@ import org.adeveloper.expensemanager.EventListener.RealtimeUserBalance;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ManageResourcesActivity extends Activity {
+	
+	public String balanceTypeValue = CalculateBalance.BALANCE_INCREASE;
+	
+	private TextView realtimeBalance;
+	private static final String KEY_REALTIME_BALANCE = "realtimebalance";
+	
+	public String balanceTypeSign = "+";
+	public static final String KEY_BALANCE_TYPE_SIGN = "balancetypesign";
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +28,87 @@ public class ManageResourcesActivity extends Activity {
 		
 		setContentView(R.layout.activity_manage_resources);
 		
-		addBalance();
-		subtractBalance();
-	}
-	
-	private void addBalance()
-	{
-		final TextView realtimeNewBalance = (TextView) findViewById(R.id.realtime_new_balance);
-		final EditText newBalanceField = (EditText) findViewById(R.id.add_new_balance_field);
-		newBalanceField.setOnKeyListener(new RealtimeUserBalance(newBalanceField, realtimeNewBalance));
-		
-		Button addNewBalancButton = (Button) findViewById(R.id.add_new_balance_button);
-		addNewBalancButton.setOnClickListener(new CalculateBalance(this, CalculateBalance.BALANCE_INCREASE, newBalanceField));
+		manageBalance(savedInstanceState);
 		
 	}
 	
-	private void subtractBalance()
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
 	{
-		final TextView realtimeValue = (TextView) findViewById(R.id.realtime_subtraction_balance);
-		final EditText userBalance = (EditText) findViewById(R.id.subtract_current_balance_field);
-		userBalance.setOnKeyListener(new RealtimeUserBalance(userBalance, realtimeValue));
-		
-		Button button = (Button) findViewById(R.id.subtract_current_balance_button);
-		button.setOnClickListener(new CalculateBalance(this, CalculateBalance.BALANCE_DECREASE, userBalance));
+		super.onSaveInstanceState(outState);
+		outState.putString(KEY_REALTIME_BALANCE, realtimeBalance.getText().toString());
+		outState.putString(KEY_BALANCE_TYPE_SIGN, balanceTypeSign);
 	}
+
+	
+	private void manageBalance(Bundle savedInstanceState)
+	{
+		realtimeBalance = (TextView) findViewById(R.id.realtime_balance);
+		
+		if(savedInstanceState != null)
+		{
+			realtimeBalance.setText(savedInstanceState.getString(KEY_REALTIME_BALANCE));
+			
+			setBalanceType(savedInstanceState.getString(KEY_BALANCE_TYPE_SIGN));
+		}
+		
+		EditText price = (EditText) findViewById(R.id.price_field);
+		price.setOnKeyListener(new RealtimeUserBalance(price, realtimeBalance));
+		
+		Button button = (Button) findViewById(R.id.balance_button);
+		button.setOnClickListener(new CalculateBalance(this, balanceTypeValue, price, realtimeBalance));
+	}
+	
+	private void setBalanceType(String sign)
+	{
+		if(sign.contains("+"))
+		{
+			setBalanceTypeIncrease();
+		}else if(sign.contains("-"))
+		{
+			setBalanceTypeDecrease();
+		}
+	}
+	
+	public void flipBalanceType(View view)
+	{
+		ImageButton balanceType = (ImageButton) findViewById(R.id.balance_type_image);
+		
+		if(balanceType.getTag().toString().contains("+")){
+			setBalanceTypeDecrease();
+		}else if(balanceType.getTag().toString().contains("-")){
+			setBalanceTypeIncrease();
+		}
+		
+		// Bind event again. (will take balaneTypeValue property's newly set value).
+		manageBalance(null);
+	}
+	
+	private void setBalanceTypeIncrease()
+	{
+		balanceTypeSign = "+";
+
+		TextView balanceText = (TextView) findViewById(R.id.balance_text);
+		ImageButton balanceType = (ImageButton) findViewById(R.id.balance_type_image);
+		
+		balanceType.setTag(balanceTypeSign);
+		balanceTypeValue = CalculateBalance.BALANCE_INCREASE;
+		balanceType.setImageDrawable(getResources().getDrawable(R.drawable.plus_sign));
+		balanceText.setText("افزایش موجودی:");
+	}
+	
+	private void setBalanceTypeDecrease()
+	{
+		balanceTypeSign = "-";
+		
+		TextView balanceText = (TextView) findViewById(R.id.balance_text);
+		ImageButton balanceType = (ImageButton) findViewById(R.id.balance_type_image);
+		
+		balanceType.setTag(balanceTypeSign);
+		balanceTypeValue = CalculateBalance.BALANCE_DECREASE;
+		balanceType.setImageDrawable(getResources().getDrawable(R.drawable.minus_sign));
+		balanceText.setText("کاهش موجودی:");
+	}
+	
 }
