66366f58ad1b3996824dbe6c63b284ff11c1e245
Hossein Baghayi <hossein.baghayi@gmail.com>
2013-10-07 18:04:29 +0330

    Solve an issue about SQL query
    Also instead of returning ArrayList<> type it returns List<> type.

--- src/org/adeveloper/expensemanager/lib/Expense.java
+++ src/org/adeveloper/expensemanager/lib/Expense.java
@@ -1,13 +1,13 @@
 package org.adeveloper.expensemanager.lib;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.adeveloper.expensemanager.db.ExpensemanagerHelper;
 
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 public class Expense
 {
@@ -45,27 +45,36 @@ public class Expense
 		}
 	}
 	
-	public ArrayList<org.adeveloper.expensemanager.model.Expense> getAllExpenses()
+	public List<org.adeveloper.expensemanager.model.Expense> getAllExpenses()
 	{
-		String query = "SELECT * FROM " + ExpensemanagerHelper.TABLE_NAME_EXPENSE + 
+		String query = "SELECT "
+				+ ExpensemanagerHelper.TABLE_NAME_EXPENSE + ".Id, " 
+				+ ExpensemanagerHelper.TABLE_NAME_EXPENSE + ".Caption, " 
+				+ ExpensemanagerHelper.TABLE_NAME_BALANCE + ".NewBalance, "
+				+ ExpensemanagerHelper.TABLE_NAME_EXPENSE + ".UpdateTime "
+				+ "FROM " + ExpensemanagerHelper.TABLE_NAME_EXPENSE + 
 						" INNER JOIN " + ExpensemanagerHelper.TABLE_NAME_BALANCE +
 						" ON " + ExpensemanagerHelper.TABLE_NAME_EXPENSE + ".BalanceId=" +
 						ExpensemanagerHelper.TABLE_NAME_BALANCE + ".Id ORDER BY " +
 						ExpensemanagerHelper.TABLE_NAME_EXPENSE + ".UpdateTime DESC";
 		Cursor resultset = database.rawQuery(query, null);
 		
-		ArrayList<org.adeveloper.expensemanager.model.Expense> list = 
+		List<org.adeveloper.expensemanager.model.Expense> list = 
 				new ArrayList<org.adeveloper.expensemanager.model.Expense>();
 		
 		while (resultset.moveToNext())
 		{
 			org.adeveloper.expensemanager.model.Expense expense = 
 					new org.adeveloper.expensemanager.model.Expense();
-			expense.setCaption(resultset.getString(resultset.getColumnIndex("Caption")));
-			Log.i("Debug", expense.getCaption());
-			
-			expense.setPrice(resultset.getDouble(resultset.getColumnIndex("NewBalance")));
-			expense.setUpdateTime(resultset.getLong(resultset.getColumnIndex("UpdateTime")));
+
+			expense.setId(
+					resultset.getLong(resultset.getColumnIndex("Id")));
+			expense.setCaption(
+					resultset.getString(resultset.getColumnIndex("Caption")));
+			expense.setPrice(
+					resultset.getDouble(resultset.getColumnIndex("NewBalance")));
+			expense.setUpdateTime(
+					resultset.getLong(resultset.getColumnIndex("UpdateTime")));
 			
 			list.add(expense);
 		}
